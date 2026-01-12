c3441756eeea67c48ccce8af233eca1351dfd371
Nisha Kumari <149179235+ni-asharma@users.noreply.github.com>
2026-01-12 15:49:43 +0530

    initial commit

---
+++ Experiment-2/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+export default function ButtonBasic() {
+  return (
+    <>
+    <Button size="small" variant='outlined'>Small</Button>
+    <Button size="large" variant='contained'>Large</Button>
+    </>
+    
+  ) 
+
+}
